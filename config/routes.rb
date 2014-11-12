49e6381cbf85607f647ac571e674d2c7f295f1a5
Nuno Polonia <nuno@claan.com>
2014-11-12 12:22:56 +0000

    Translate product properties

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Spree::Core::Engine.routes.draw do
   namespace :admin do
     get '/:resource/:resource_id/translations' => 'translations#index', as: :translations
     patch '/option_values/:id' => 'option_values#update', as: :option_type_option_value
+    patch 'product/:id/product_properties/:id' => "product_properties#translate", as: :translate_product_property 
   end
 end
